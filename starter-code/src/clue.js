3ebd2332878af64462536d6a658193bb8c30d50b
Dazzsen <and24_21@msn.com>
2019-07-05 01:29:49 +0200

    done

--- starter-code/src/clue.js
+++ starter-code/src/clue.js
@@ -1,40 +1,46 @@
 // Characters
-/*
-mrGreen
-first_name:   Jacob
-last_name:    Green
-color:        green
-description:  He has a lot of connections
-age:          45
-image:        https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg
-occupation:   Entrepreneur
 
-drOrchid
-first_name:   Doctor
-last_name:    Orchid
-color:        white
-description:  PhD in plant toxicology. Adopted daughter of Mr. Boddy
-age:          26
-image:        http://www.radiotimes.com/uploads/images/Original/111967.jpg
-ocupation:   Scientist
+let mrGreen = {
+first_name:   'Jacob',
+last_name:    'Green',
+color:        'green',
+description:  'He has a lot of connections',
+age:          '45',
+image:        'https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg',
+occupation:   'Entrepreneur'
+}
 
-profPlum
-first_name:   Victor
-last_name:    Plum
-color:        purple
-description:  Billionare video game designer
-age:          22
-image:        https://metrouk2.files.wordpress.com/2016/07/professor-plum.jpg
-occupation:   Designer
+let drOrchid ={
+first_name:   'Doctor',
+last_name:    'Orchid',
+color:        'white',
+description:  'PhD in plant toxicology. Adopted daughter of Mr. Boddy',
+age:          26,
+image:        'http://www.radiotimes.com/uploads/images/Original/111967.jpg',
+ocupation:   'Scientist',
+}
 
-missScarlet
-first_name:   Kasandra
-last_name:    Scarlet
-color:        red
-description:  She is an A-list movie star with a dark past
-age:          31
-image:        https://metrouk2.files.wordpress.com/2016/07/miss-scarlett.jpg
-occupation:   Actor
+let profPlum ={
+first_name:   'Victor',
+last_name:    'Plum',
+color:        'purple',
+description:  'Billionare video game designer',
+age:          22,
+image:        'https://metrouk2.files.wordpress.com/2016/07/professor-plum.jpg',
+occupation:   'Designer'
+}
+
+
+let missScarlet ={
+first_name:   'Kasandra',
+last_name:    'Scarlet',
+color:        'red',
+description:  'She is an A-list movie star with a dark past',
+age:          31,
+image:        'https://metrouk2.files.wordpress.com/2016/07/miss-scarlett.jpg',
+occupation:   'Actor',
+}
+
 
 mrsPeacock
 first_name:   Eleanor
@@ -56,10 +62,10 @@ occupation:   Retired Football player
 
 // Weapons
 
-name: rope        ---   weight: 10
-name: knife       ---   weight: 8
-name: candlestick ---   weight: 2
-name: dumbbell    ---   weight: 30
+let rope = { weight: 10}
+let knife = {weight: 8}
+let candlestick ={ weight: 2}
+let dumbbell ={ weight: 30}
 name: poison      ---   weight: 2
 name: axe         ---   weight: 15
 name: bat         ---   weight: 13
@@ -67,10 +73,10 @@ name: trophy      ---   weight: 25
 name: pistol      ---   weight: 20
 
 // Rooms
-name: Dinning Room
-name: Conservatory
-name: Kitchen
-name: Study
+let DinningRoom
+let Conservatory
+let Kitchen
+let Study
 name: Library
 name: Billiard Room
 name: Lounge
@@ -85,10 +91,10 @@ name: Patio
 */
 
 // Characters Collection
-var charactersArray = [];
+var charactersArray = [mrGreen, drOrchid, profPlum, missScarlet];
 
 // Rooms' Collection
-var roomsArray = [];
+var roomsArray = [rope, knife, candlestick, dumbbell];
 
 // Weapons Collection
-var weaponsArray = [];
+var weaponsArray = [DinningRoom, Conservatory, Kitchen, Study];
